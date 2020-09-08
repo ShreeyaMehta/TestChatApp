77b621286c6fda6236c57440640d5c3e72d515e5
Shreeya Mehta <shreeya@bcod.co.in>
2020-09-08 09:22:39 +0530

    Commit1

--- app/src/main/java/com/bcod/testchatapp/MainActivity.java
+++ app/src/main/java/com/bcod/testchatapp/MainActivity.java
@@ -2,11 +2,14 @@ package com.bcod.testchatapp;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.app.Activity;
 import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -40,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private ArrayList<String> chatMessages;
     private BluetoothAdapter bluetoothAdapter;
     public ArrayList<BluetoothDevice> bluetoothDevices = new ArrayList<>();
+    FragmentActivity fragmentActivity;
 
 
     public static final int MESSAGE_STATE_CHANGE = 1;
@@ -52,6 +56,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private static final int REQUEST_ENABLE_BLUETOOTH = 1;
     private ChatController chatController;
     private BluetoothDevice connectingDevice;
+    BluetoothClass bluetoothClass;
     private ArrayAdapter<String> discoveredDevicesAdapter;
 
 
@@ -143,103 +148,107 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         IntentFilter filter1 = new IntentFilter(BluetoothDevice.ACTION_BOND_STATE_CHANGED);
         registerReceiver(mBroadcastReceiver4, filter1);
 
-/*
-        if (bluetoothAdapter.isDiscovering()) {
+        bluetoothClass = connectingDevice.getBluetoothClass();
+
+    //    if (bluetoothClass.getDeviceClass() == BluetoothClass.Device.PHONE_SMART) {
+            if (bluetoothAdapter.isDiscovering()) {
+                bluetoothAdapter.cancelDiscovery();
+
+                //check BT permissions in manifest
+                checkBTPermissions();
+
+                bluetoothAdapter.startDiscovery();
+                Toast.makeText(this, "Searching...", Toast.LENGTH_LONG).show();
+                IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                registerReceiver(discoveryFinishReceiver, discoverDevicesIntent);
+            }
+            if (!bluetoothAdapter.isDiscovering()) {
+
+                //check BT permissions in manifest
+                checkBTPermissions();
+
+                bluetoothAdapter.startDiscovery();
+                Toast.makeText(this, "Searching...", Toast.LENGTH_LONG).show();
+                IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                registerReceiver(discoveryFinishReceiver, discoverDevicesIntent);
+            }
+
+            //Initializing bluetooth adapters
+            ArrayAdapter<String> pairedDevicesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+            discoveredDevicesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+
+            //locate listviews and attatch the adapters
+            ListView listView = (ListView) dialog.findViewById(R.id.pairedDeviceList);
+            ListView listView2 = (ListView) dialog.findViewById(R.id.discoveredDeviceList);
+            listView.setAdapter(pairedDevicesAdapter);
+            listView2.setAdapter(discoveredDevicesAdapter);
+            listView2.setOnItemClickListener(MainActivity.this);
+
+            // Register for broadcasts when a device is discovered
+            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(discoveryFinishReceiver, filter);
+
+            // Register for broadcasts when discovery has finished
+            filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+            registerReceiver(discoveryFinishReceiver, filter);
+
+            bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+            // If there are paired devices, add each one to the ArrayAdapter
+            if (pairedDevices.size() > 0 && bluetoothClass.getDeviceClass() == BluetoothClass.Device.PHONE_SMART) {
+                for (BluetoothDevice device : pairedDevices) {
+                    pairedDevicesAdapter.add(device.getName() + "\n" + device.getAddress());
+                }
+            } else {
+                pairedDevicesAdapter.add(getString(R.string.none_paired));
+            }
+
+/*        if (bluetoothAdapter.isDiscovering()) {
             bluetoothAdapter.cancelDiscovery();
         }
         bluetoothAdapter.startDiscovery();
 */
-        if(bluetoothAdapter.isDiscovering()){
-            bluetoothAdapter.cancelDiscovery();
 
-            //check BT permissions in manifest
-            checkBTPermissions();
 
-            bluetoothAdapter.startDiscovery();
-            Toast.makeText(this, "Searching...", Toast.LENGTH_LONG).show();
-            IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-            registerReceiver(discoveryFinishReceiver, discoverDevicesIntent);
+            //Handling listview item click event
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    bluetoothAdapter.cancelDiscovery();
+                    String info = ((TextView) view).getText().toString();
+                    String address = info.substring(info.length() - 17);
+
+                    connectToDevice(address);
+                    dialog.dismiss();
+                }
+
+            });
+
+            listView2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    bluetoothAdapter.cancelDiscovery();
+                    String info = ((TextView) view).getText().toString();
+                    String address = info.substring(info.length() - 17);
+
+                    connectToDevice(address);
+                    dialog.dismiss();
+                }
+            });
+
+            dialog.findViewById(R.id.cancelButton).setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+            dialog.setCancelable(false);
+            dialog.show();
         }
-        if(!bluetoothAdapter.isDiscovering()){
-
-            //check BT permissions in manifest
-            checkBTPermissions();
-
-            bluetoothAdapter.startDiscovery();
-            Toast.makeText(this, "Searching...", Toast.LENGTH_LONG).show();
-            IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-            registerReceiver(discoveryFinishReceiver, discoverDevicesIntent);
-        }
-
-        //Initializing bluetooth adapters
-        ArrayAdapter<String> pairedDevicesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
-        discoveredDevicesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
-
-        //locate listviews and attatch the adapters
-        ListView listView = (ListView) dialog.findViewById(R.id.pairedDeviceList);
-        ListView listView2 = (ListView) dialog.findViewById(R.id.discoveredDeviceList);
-        listView.setAdapter(pairedDevicesAdapter);
-        listView2.setAdapter(discoveredDevicesAdapter);
-        listView2.setOnItemClickListener(MainActivity.this);
-
-
-        // Register for broadcasts when a device is discovered
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(discoveryFinishReceiver, filter);
-
-        // Register for broadcasts when discovery has finished
-        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-        registerReceiver(discoveryFinishReceiver, filter);
-
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-
-        // If there are paired devices, add each one to the ArrayAdapter
-        if (pairedDevices.size() > 0) {
-            for (BluetoothDevice device : pairedDevices) {
-                pairedDevicesAdapter.add(device.getName() + "\n" + device.getAddress());
-            }
-        } else {
-            pairedDevicesAdapter.add(getString(R.string.none_paired));
-        }
-
-        //Handling listview item click event
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                bluetoothAdapter.cancelDiscovery();
-                String info = ((TextView) view).getText().toString();
-                String address = info.substring(info.length() - 17);
-
-                connectToDevice(address);
-                dialog.dismiss();
-            }
-
-        });
-
-        listView2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                bluetoothAdapter.cancelDiscovery();
-                String info = ((TextView) view).getText().toString();
-                String address = info.substring(info.length() - 17);
-
-                connectToDevice(address);
-                dialog.dismiss();
-            }
-        });
-
-        dialog.findViewById(R.id.cancelButton).setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-        });
-        dialog.setCancelable(false);
-        dialog.show();
-    }
+    //}
 
     private void setStatus(String s) {
         status.setText(s);
